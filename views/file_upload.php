81f85d4d7edee12c02fc8bb218f37ad3e9be0a10
Dev-Tams <tammyalemu@gmail.com>
2024-10-17 03:29:23 +0100

    added views page for file upload

---
+++ views/file_upload.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File Upload</title>
+</head>
+<body>
+    <h2>Upload a File</h2>
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+    <label for="image">Upload an Image:</label>
+    <input type="file" name="image" id="image" required><br><br>
+
+    <label for="audio">Upload an Audio File:</label>
+    <input type="file" name="audio" id="audio" required><br><br>
+
+    <label for="video">Upload a Video File:</label>
+    <input type="file" name="video" id="video" required><br><br>
+
+    <label for="document">Upload a Document:</label>
+    <input type="file" name="document" id="document" required><br><br>
+
+    <input type="submit" value="Upload File">
+</form>
+</body>
+</html>
